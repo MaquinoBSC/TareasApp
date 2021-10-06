b15faa7c1342c6883e635590b03f84a91535ceaa
MaquinoBSC <martinare.ipn@gmail.com>
2021-10-06 00:35:31 -0500

    Agregar funcionalidad para eliminar task

--- index.js
+++ index.js
@@ -10,31 +10,39 @@ const saveTask= (title, description)=> {
     });
 }
 
-const getTasks= ()=> db.collection('tasks').get();
+const onGetTasks= (callback)=> db.collection('tasks').onSnapshot(callback);
 
-const onGetTasks= (callback)=> db.collection('tasks').onSnapshot(callback)
+const deleteTask= (id)=> db.collection('tasks').doc(id).delete();
 
 
 window.addEventListener('DOMContentLoaded', async(e)=> {
     onGetTasks((querySnapshot)=> {
+
         tasksContainer.innerHTML= "";
-        
-        querySnapshot.forEach(doc=> {
+
+        querySnapshot.forEach((doc)=> {
             console.log(doc.data());
             const task= doc.data();
+            task.id= doc.id;
     
             tasksContainer.innerHTML += `
             <div class="card card-body mt-2 border-primary">
                 <h3 class="h5">${task.title}</h3>
                 <p>${task.description}</p>
                 <div>
-                    <button class="btn btn-warning">Delete</button>
+                    <button class="btn btn-warning btn-delete" data-id=${doc.id}>Delete</button>
                     <button class="btn btn-success">Edit</button>
                 </div>
             </div>`
+
+            const btnsDelete= document.querySelectorAll('.btn-delete');
+            btnsDelete.forEach((btn)=> {
+                btn.addEventListener('click', async(e)=> {
+                    await deleteTask(e.target.dataset.id);
+                });
+            });
         });
     });
-
 });
 
 
