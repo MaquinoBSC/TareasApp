2e0adff8f1513caef06db3b947be7c928422b5b5
MaquinoBSC <martinare.ipn@gmail.com>
2021-10-05 22:17:17 -0500

    agergar SDK del servicio Firestore y guardar datos

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+const db= firebase.firestore();
+
 const taskForm= document.querySelector('#task-form');
-taskForm.addEventListener('submit', (e)=> {
+taskForm.addEventListener('submit', async(e)=> {
     e.preventDefault();
 
     const title= taskForm['task-title'].value;
     const description= taskForm['task-description'].value;
-})
+
+    const response= await db.collection('tasks').doc().set({
+        title,
+        description
+    });
+
+    console.log(response);
+});
