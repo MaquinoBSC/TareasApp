72a031b68db50f2bd62516d06d5d4bf6b3eb36e8
MaquinoBSC <martinare.ipn@gmail.com>
2021-10-05 23:34:35 -0500

    Construir template y obtener todas las tasks, ademas de agregar listener event que detecta si se agrega o editar un documento de la collecion tasks

--- index.js
+++ index.js
@@ -1,16 +1,53 @@
 const db= firebase.firestore();
 
 const taskForm= document.querySelector('#task-form');
-taskForm.addEventListener('submit', async(e)=> {
-    e.preventDefault();
+const tasksContainer= document.getElementById('tasks-container');
 
-    const title= taskForm['task-title'].value;
-    const description= taskForm['task-description'].value;
-
-    const response= await db.collection('tasks').doc().set({
+const saveTask= (title, description)=> {
+    return db.collection('tasks').doc().set({
         title,
         description
     });
+}
 
-    console.log(response);
+const getTasks= ()=> db.collection('tasks').get();
+
+const onGetTasks= (callback)=> db.collection('tasks').onSnapshot(callback)
+
+
+window.addEventListener('DOMContentLoaded', async(e)=> {
+    onGetTasks((querySnapshot)=> {
+        tasksContainer.innerHTML= "";
+        
+        querySnapshot.forEach(doc=> {
+            console.log(doc.data());
+            const task= doc.data();
+    
+            tasksContainer.innerHTML += `
+            <div class="card card-body mt-2 border-primary">
+                <h3 class="h5">${task.title}</h3>
+                <p>${task.description}</p>
+                <div>
+                    <button class="btn btn-warning">Delete</button>
+                    <button class="btn btn-success">Edit</button>
+                </div>
+            </div>`
+        });
+    });
+
+});
+
+
+
+taskForm.addEventListener('submit', async(e)=> {
+    e.preventDefault();
+
+    const title= taskForm['task-title'];
+    const description= taskForm['task-description'];
+
+    await saveTask(title.value, description.value);
+    title.focus();
+
+    taskForm.reset();
+
 });
