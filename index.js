e5465822a16d08c7f73d7d75586c7a114c24bfcc
MaquinoBSC <martinare.ipn@gmail.com>
2021-10-05 21:40:32 -0500

    Crear template usando bootswatch

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("works!!");
+const taskForm= document.querySelector('#task-form');
+taskForm.addEventListener('submit', (e)=> {
+    e.preventDefault();
+
+    const title= taskForm['task-title'].value;
+    const description= taskForm['task-description'].value;
+})
