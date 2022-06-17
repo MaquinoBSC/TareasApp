c531f395c59ee54034a1d27746b5b749da0c5201
MaquinoBSC <martinare.ipn@gmail.com>
2022-06-16 23:38:49 -0500

    Se agregan funciones para obtener datos (getDocs y onSnapshot)

--- firebase-v9.js
+++ firebase-v9.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-app.js";
-import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-firestore.js"
+import { getFirestore, collection, addDoc, getDocs, onSnapshot } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-firestore.js"
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
@@ -18,4 +18,11 @@ const app = initializeApp(firebaseConfig);
 // Initialize Firestore
 const db= getFirestore();
 
+// Guardar una tarea en firestore
 export const saveTask= ( title, description )=> addDoc(collection( db, 'tasksV9' ), { title, description });
+
+// Obtener todas las tareas
+export const getTasks= ()=> getDocs( collection( db, 'tasksV9' ) );
+
+//Obtener los datos en tiempo real
+export const onGetTasks= ( callback )=> onSnapshot( collection( db, 'tasksV9'), callback );
--- index-v9.js
+++ index-v9.js
@@ -1,11 +1,29 @@
-import { saveTask } from './firebase-v9.js';
+import { getTasks, saveTask, onGetTasks } from './firebase-v9.js';
+
+// Referencias HTML
+const tasksContainer= document.getElementById('tasks-container');
+const taskForm= document.getElementById('task-form');
+
 
 // Ejecutar un evento cuando la aplicacion cargue
-window.addEventListener('DOMContentLoaded', ()=> {
+window.addEventListener('DOMContentLoaded', async ()=> {
+    onGetTasks(( querySnapshot )=> {
+        let html= '';
+        
+        querySnapshot.forEach( doc => {
+            const { title, description }= doc.data();
+            html+= `
+                <div>
+                    <h3> ${ title } </h3>
+                    <p> ${ description } </p>
+                </div>
+            `;
+        });
     
+        tasksContainer.innerHTML= html;
+    });
 });
 
-const taskForm= document.getElementById('task-form');
 taskForm.addEventListener('submit', ( e )=> {
     e.preventDefault();
 
